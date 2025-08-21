22ef7787d43faa2adfed026791945501033745d4
dlywlotus <dylanlowywgit@gmail.com>
2025-08-21 17:21:58 +0800

    finish Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final int LINE_BREAK_LENGTH = 45;
+    private static ArrayList<String> taskList = new ArrayList<>();
 
     private static void printLineBreak() {
         for (int i = 0; i < LINE_BREAK_LENGTH; i++) {
             System.out.print('-');
         }
         System.out.print("\n");
+    }
+
+    private static void addTask(String task) {
+        taskList.add(task);
+        System.out.println("added: " + task);
+        printLineBreak();
+    }
+
+    private static void printTasks() {
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, taskList.get(i));
+        }
+        System.out.println();
+        printLineBreak();
     }
 
     public static void main(String[] args) {
@@ -17,15 +33,17 @@ public class Duke {
         System.out.println("What can I do for you?");
         printLineBreak();
         while(true) {
-            String input = scanner.next();
+            String input = scanner.nextLine();
             switch (input) {
+                case "list":
+                    printTasks();
+                    break;
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     printLineBreak();
                     return;
                 default:
-                    System.out.println(input);
-                    printLineBreak();
+                    addTask(input);
             }
         }
 
