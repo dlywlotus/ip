e5d2bf3bfde1cebf26b1c8940a0b75a7f3d4c524
dlywlotus <dylanlowywgit@gmail.com>
2025-08-21 16:31:22 +0800

    finish level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
+    private static final int LINE_BREAK_LENGTH = 80;
+
+    private static void printLineBreak() {
+        for (int i = 0; i < LINE_BREAK_LENGTH; i++) {
+            System.out.print('-');
+        }
+        System.out.print("\n");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLineBreak();
+        System.out.println("Hello! I'm Alan");
+        System.out.println("What can I do for you?");
+        printLineBreak();
+        System.out.println("Bye. See you soon!");
+        printLineBreak();
     }
 }
