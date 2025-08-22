6474c3287bb4a86d5aaf59b7e4b2feb8bcfced9b
dlywlotus <dylanlowywgit@gmail.com>
2025-08-22 12:49:44 +0800

    finish Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final int LINE_BREAK_LENGTH = 45;
-    private static ArrayList<String> taskList = new ArrayList<>();
+    private static ArrayList<Task> taskList = new ArrayList<>();
 
     private static void printLineBreak() {
         for (int i = 0; i < LINE_BREAK_LENGTH; i++) {
@@ -12,17 +13,28 @@ public class Duke {
         System.out.print("\n");
     }
 
-    private static void addTask(String task) {
+    private static void addTask(Task task) {
         taskList.add(task);
-        System.out.println("added: " + task);
+        System.out.println("added: " + task.description);
+        printLineBreak();
+    }
+
+    private static void markTask(int taskId) {
+        if (taskId > taskList.size() || taskId < 1) {
+            throw new InvalidParameterException("The task does not exist");
+        }
+        Task task = taskList.get(taskId - 1);
+        task.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.printf("  %s %s\n", task.getStatusIcon(), task.description);
         printLineBreak();
     }
 
     private static void printTasks() {
         for (int i = 0; i < taskList.size(); i++) {
-            System.out.printf("%d. %s\n", i + 1, taskList.get(i));
+            Task task = taskList.get(i);
+            System.out.printf("%d.%s %s\n", i + 1, task.getStatusIcon(), task.description);
         }
-        System.out.println();
         printLineBreak();
     }
 
@@ -34,6 +46,21 @@ public class Duke {
         printLineBreak();
         while(true) {
             String input = scanner.nextLine();
+            String[] inputWords = input.split(" ");
+
+            if (inputWords.length > 0 && inputWords[0].equals("mark")) {
+                try {
+                    if (inputWords.length < 2) {
+                        throw new IllegalArgumentException("Missing task index");
+                    }
+                    String secondWord = inputWords[1];
+                    markTask(Integer.parseInt(secondWord));
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+                continue;
+            }
+
             switch (input) {
                 case "list":
                     printTasks();
@@ -43,7 +70,7 @@ public class Duke {
                     printLineBreak();
                     return;
                 default:
-                    addTask(input);
+                    addTask(new Task(input));
             }
         }
 
