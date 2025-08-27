b1c989f0363564e776b39fa1205ba6154f8c70c2
dlywlotus <dylanlowywgit@gmail.com>
2025-08-27 14:52:58 +0800

    Tweak code to comply with coding standdard

--- src/main/java/com/alanthechatbot/parse/Parser.java
+++ src/main/java/com/alanthechatbot/parse/Parser.java
@@ -1,9 +1,9 @@
 package com.alanthechatbot.parse;
 
-import com.alanthechatbot.exceptions.InputParsingException;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import com.alanthechatbot.exceptions.InputParsingException;
 
 public class Parser {
     private final String inputLine;
@@ -25,55 +25,55 @@ public class Parser {
         int index = 0;
 
         switch (firstWord) {
-            case "todo":
-                i = 1;
-                while (i < inputWords.size()) {
-                    taskDesc.add(inputWords.get(i));
-                    i += 1;
-                }
-                break;
-            case "deadline":
-                i = 1;
-                while (i < inputWords.size() && !inputWords.get(i).equals("/by")) {
-                    taskDesc.add(inputWords.get(i));
-                    i += 1;
-                }
-                for (int j = i + 1; j < inputWords.size(); j++) {
-                    doneBy.add(inputWords.get(j));
-                }
-                break;
-            case "event":
-                taskDesc = new ArrayList<>();
-                i = 1;
-                while (i < inputWords.size() && !inputWords.get(i).equals("/from")) {
-                    taskDesc.add(inputWords.get(i));
-                    i += 1;
-                }
+        case "todo":
+            i = 1;
+            while (i < inputWords.size()) {
+                taskDesc.add(inputWords.get(i));
                 i += 1;
-                while (i < inputWords.size() && !inputWords.get(i).equals("/to")) {
-                    from.add(inputWords.get(i));
-                    i += 1;
-                }
-                for (int j = i + 1; j < inputWords.size(); j++) {
-                    to.add(inputWords.get(j));
-                }
-                break;
-            case "mark":
-            case "delete":
-                if (inputWords.size() < 2) {
-                    throw new InputParsingException("Please provide the task id!");
-                }
-                try {
-                    index = Integer.parseInt(inputWords.get(1));
-                } catch (NumberFormatException e) {
-                    throw new InputParsingException("The task id should be an integer!");
-                }
-                break;
-            case "list":
-            case "bye":
-                break;
-            default:
-                firstWord = "invalid input";
+            }
+            break;
+        case "deadline":
+            i = 1;
+            while (i < inputWords.size() && !inputWords.get(i).equals("/by")) {
+                taskDesc.add(inputWords.get(i));
+                i += 1;
+            }
+            for (int j = i + 1; j < inputWords.size(); j++) {
+                doneBy.add(inputWords.get(j));
+            }
+            break;
+        case "event":
+            taskDesc = new ArrayList<>();
+            i = 1;
+            while (i < inputWords.size() && !inputWords.get(i).equals("/from")) {
+                taskDesc.add(inputWords.get(i));
+                i += 1;
+            }
+            i += 1;
+            while (i < inputWords.size() && !inputWords.get(i).equals("/to")) {
+                from.add(inputWords.get(i));
+                i += 1;
+            }
+            for (int j = i + 1; j < inputWords.size(); j++) {
+                to.add(inputWords.get(j));
+            }
+            break;
+        case "mark":
+        case "delete":
+            if (inputWords.size() < 2) {
+                throw new InputParsingException("Please provide the task id!");
+            }
+            try {
+                index = Integer.parseInt(inputWords.get(1));
+            } catch (NumberFormatException e) {
+                throw new InputParsingException("The task id should be an integer!");
+            }
+            break;
+        case "list":
+        case "bye":
+            break;
+        default:
+            firstWord = "invalid input";
         }
         return new ParsedInput(firstWord,
                 String.join(" ", taskDesc),
